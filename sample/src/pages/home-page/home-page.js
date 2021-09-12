908f52ce2df08b7fbb71c928d4688b8197335938
barisyilirim06 <yildrmbaris@gmail.com>
2021-09-12 13:49:23 +0300

    Add home-page to display views

---
+++ sample/src/pages/home-page/home-page.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { tasksData, statusesData } from 'initialData.js';
+import './home-page.scss';
+import BoardView from 'views/board-view';
+
+export default function HomePage() {
+    const [ tasks, setTasks ] = useState(tasksData);
+    const [ statuses, setStatuses ] = useState(statusesData);
+
+    return (
+        <div>
+            <BoardView 
+                statuses={statuses}
+                tasks={tasks}
+            />
+        </div>
+    )
+}
+
