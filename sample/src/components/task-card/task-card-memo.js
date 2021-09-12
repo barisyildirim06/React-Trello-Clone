c86d26029ec86cd5b26b61d708ac14f4edbcd54e
barisyilirim06 <yildrmbaris@gmail.com>
2021-09-12 13:45:13 +0300

    Add task-card component

---
+++ sample/src/components/task-card/task-card-memo.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import TaskCard from './task-card';
+
+export default React.memo(TaskCard);
---
+++ sample/src/components/task-card/task-card.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './task-card.scss';
+
+import { Tooltip } from 'antd';
+
+/* Components */
+import Image from 'components/image';
+// import Spacer from 'components/spacer';
+
+export default function TaskCard({ task, color }) {
+    let statusColor = "rgb(0,0,0)";
+    let statusText = "";
+    let title = "";
+
+    if (task) {
+        title = `${task.title} - ${task.id}`;
+
+        if (task.status) {
+            statusText = task?.status?.text || "";
+        }
+    }
+
+    if (color) {
+        statusColor = color;
+    }
+
+    return (
+        <div className="task-card" >
+            <div className="task-card-icon" style={{ borderColor: statusColor, padding: "3px"}}>
+                <Image src={"/static/img/checkmark.png"} />
+            </div>
+            <div className="task-card-details">
+                <div style={{ display: "flex" }}>
+                    <div className="task-card-title">{title}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
