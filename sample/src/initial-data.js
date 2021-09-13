0a9c811ea7800231eec64fc5b6c99b729370bd36
barisyilirim06 <yildrmbaris@gmail.com>
2021-09-13 16:59:54 +0300

    Initialize the task tags data

--- sample/src/initial-data.js
+++ sample/src/initial-data.js
@@ -15,33 +15,50 @@ const tasksData = [
     {
         id: "1",
         status: 'Not Inspected',
-        title: 'new task 1'
+        title: 'new task 1',
+        tags: [{ value: '#01e164', label: 'Test' }]
     },
     {
         id: "2",
         status: 'Not Inspected',
-        title: 'new task 2'
+        title: 'new task 2',
+        tags: [{ value: '#01e164', label: 'Test' }, { value: '#46d0ff', label: 'Feedback' }]
     },
     {
         id: "3",
         status: 'Not Inspected',
-        title: 'new task 3'
+        title: 'new task 3',
+        tags: [{ value: '#01e164', label: 'Test' }]
     },
     {
         id: "4",
         status: 'Pass',
-        title: 'new task 4'
+        title: 'new task 4',
+        tags: [{ value: '#ffd100', label: 'Idea' }]
     },
     {
         id: "5",
         status: 'Pass',
-        title: 'new task 5'
+        title: 'new task 5',
+        tags: [{ value: '#01e164', label: 'Test' }, { value: '#6d62e2', label: 'Design' }]
     },
     {
         id: "6",
         status: 'Pass',
-        title: 'new task 6'
+        title: 'new task 6',
+        tags: [{ value: '#01e164', label: 'Test' }]
     },
 ]
 
-export { statusesData, tasksData };
+const tagValues = [
+    { value: '#fd2061', label: 'Bug' }, 
+    { value: '#6d62e2', label: 'Design' }, 
+    { value: '#eeeeed', label: 'Dev' }, 
+    { value: '#46d0ff', label: 'Feedback' }, 
+    { value: '#ffd100', label: 'Idea' }, 
+    { value: '#0086ff', label: 'Marketing' }, 
+    { value: '#01e164', label: 'Test' }
+]
+
+export 
+{ statusesData, tasksData, tagValues };
