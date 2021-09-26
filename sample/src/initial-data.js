94ac3e878b75fe2f849cd13dbc13a015542eb75e
barisyilirim06 <yildrmbaris@gmail.com>
2021-09-26 23:26:04 +0300

    Modify initial data

--- sample/src/initial-data.js
+++ sample/src/initial-data.js
@@ -23,7 +23,7 @@ const tasksData = [
         statusID: '1',
         title: 'Tanzania',
         comments: 'country in East Africa within the African Great Lakes region',
-        tags: [{ value: '#0086ff', label: 'Marketing' }]
+        tags: [ { value: '#9e0e15', label: 'Trivial' }, { value: '#b5c497', label: 'Test' }, { value: '#ffd100', label: 'Console' }]
     },
     {
         id: "2",
@@ -31,7 +31,7 @@ const tasksData = [
         statusID: '1',
         title: 'Slovenia',
         comments: 'Slovenia is a country in Southern Europe',
-        tags: [{ value: '#01e164', label: 'Test' }, { value: '#46d0ff', label: 'Feedback' }]
+        tags: [{ value: '#c497b8', label: 'Major' }, { value: '#c4b797', label: 'Beta' } ,{ value: '#46d0ff', label: 'App' }]
     },
     {
         id: "3",
@@ -39,7 +39,7 @@ const tasksData = [
         statusID: '1',
         title: 'Laos',
         comments: 'Cool',
-        tags: [{ value: '#01e164', label: 'Test' }, { value: '#6d62e2', label: 'Design' },{ value: '#01e164', label: 'Test' }, { value: '#46d0ff', label: 'Feedback' }]
+        tags: [{ value: '#6d62e2', label: 'Minor' }, { value: '#ffd100', label: 'Console' },{ value: '#b5c497', label: 'Test' }]
     },
     {
         id: "4",
@@ -47,7 +47,7 @@ const tasksData = [
         status: 'In Progress',
         title: 'Singapore',
         comments: 'The English name of "Singapore" is an anglicisation of the native Malay name for the country, Singapura, which was in turn derived from the Sanskrit word',
-        tags: [{ value: '#fd2061', label: 'Bug' }]
+        tags: [{ value: '#9e0e15', label: 'Trivial' }]
     },
     {
         id: "5",
@@ -55,7 +55,7 @@ const tasksData = [
         status: 'In Progress',
         title: 'Ecuador',
         comments: `he country's name means "Equator" in Spanish, truncated from the Spanish official name, República del Ecuador`,
-        tags: [{ value: '#ffd100', label: 'Idea' }]
+        tags: [{ value: '#ffd100', label: 'Console' }]
     },
     {
         id: "6",
@@ -63,7 +63,7 @@ const tasksData = [
         status: 'In Progress',
         title: 'Argentina',
         comments: 'Argentina covers an area of 2,780,400 km2 (1,073,500 sq mi), making it the largest Spanish-speaking nation in the world',
-        tags: [{ value: '#ffd100', label: 'Idea' }, { value: '#fd2061', label: 'Bug' }]
+        tags: [{ value: '#ffd100', label: 'Console' }, { value: '#9e0e15', label: 'Trivial' }]
     },
     {
         id: "7",
@@ -71,7 +71,7 @@ const tasksData = [
         status: 'Pass',
         title: 'Hungary',
         comments: 'Hungary is a landlocked country. Hungary has a population of 10 million, mostly ethnic Hungarians and a significant Romani minority',
-        tags: [{ value: '#01e164', label: 'Test' }, { value: '#6d62e2', label: 'Design' }]
+        tags: [{ value: '#01e164', label: 'Test' }, { value: '#6d62e2', label: 'Minor' }]
     },
     {
         id: "8",
@@ -79,7 +79,7 @@ const tasksData = [
         status: 'Pass',
         title: 'Costa Rica',
         comments: 'Costa Rica borders the Caribbean Sea to the east, and the Pacific Ocean to the west',
-        tags: [{ value: '#46d0ff', label: 'Feedback' }]
+        tags: [{ value: '#46d0ff', label: 'App' }]
     },
     {
         id: "9",
@@ -92,13 +92,17 @@ const tasksData = [
 ]
 
 const tagValues = [
-    { value: '#fd2061', label: 'Bug' }, 
-    { value: '#6d62e2', label: 'Design' }, 
-    { value: '#eeeeed', label: 'Dev' }, 
-    { value: '#46d0ff', label: 'Feedback' }, 
-    { value: '#ffd100', label: 'Idea' }, 
-    { value: '#0086ff', label: 'Marketing' }, 
-    { value: '#01e164', label: 'Test' }
+    { value: '#9e0e15', label: 'Trivial' }, 
+    { value: '#c497b8', label: 'Major' }, 
+    { value: '#6d62e2', label: 'Minor' }, 
+    { value: '#97c499', label: 'Prod' }, 
+    { value: '#b5c497', label: 'Test' }, 
+    { value: '#c4b797', label: 'Beta' }, 
+    { value: '#c4a697', label: 'Dev' }, 
+    { value: '#46d0ff', label: 'App' }, 
+    { value: '#ffd100', label: 'Console' }, 
+    { value: '#0086ff', label: 'Todos' }, 
+    { value: '#97c4b9', label: 'Project' }, 
 ]
 
 export 
